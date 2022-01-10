d7f0f65b75ea5a7754afff3a206824c1bc48d078
kilonzi <vkilonzi05@gmail.com>
2022-01-10 13:01:02 +0300

    created a login view function

--- neighbourhod/views.py
+++ neighbourhod/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+def loginPage(request):
+    page = 'login'
+
+    if request.user.is_authenticated:
+        return redirect('homepage')
+
+
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try: 
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'user doesnt exist')
+
+        user = authenticate(request, username= username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('homepage')
+        else:
+            messages.error(request, 'username or password does not exist')
+
+    context = {
+        'page':page
+    }
+    return render(request, 'registration/login.html', context)
